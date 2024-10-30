e74d09bafc41a3ef5c02df777b4c7f1c893a7867
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-10-30 21:44:58 +0900

    24.10.30 : 1차원 배열

--- java-start/src/array/Array1Ref2.java
+++ java-start/src/array/Array1Ref2.java
@@ -1,12 +1,13 @@
 package array;
 
-public class Array1Ref1 {
+public class Array1Ref2 {
     public static void main(String[] args) {
 
         int[] students;
         students = new int[5];
 
         System.out.println(students);
+        System.out.println(students.length);
 
         // 변수 값 대입
         students[0] = 90;
@@ -17,11 +18,9 @@ public class Array1Ref1 {
         // 오류
 //        students[5] = 50;
 
+        for (int i = 0; i < students.length; i++) {
+            System.out.println("학생" + (i+1) + "점수" + students[i]);
+        }
 
-        System.out.println("학생1 점수:" + students[0]);
-        System.out.println("학생2 점수:" + students[1]);
-        System.out.println("학생3 점수:" + students[2]);
-        System.out.println("학생4 점수:" + students[3]);
-        System.out.println("학생5 점수:" + students[4]);
     }
 }
