2da453adb203f35d210530c5edf4091fdc8dac70
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-11-02 15:52:02 +0900

    24.11.02 : 2차원 배열 리펙토링

--- java-start/src/array/ArrayDi4.java
+++ java-start/src/array/ArrayDi4.java
@@ -1,12 +1,15 @@
 package array;
 
-public class ArrayDi2 {
+public class ArrayDi4 {
     public static void main(String[] args) {
-        int[][] arr = {
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9}
-        };
+        int[][] arr = new int[3][3];
+
+        int i = 1;
+        for (int row = 0; row < arr.length; row++) {
+            for (int coloum = 0; coloum < arr[row].length; coloum++) {
+                arr[row][coloum] = i++;
+            }
+        }
 
         for (int row = 0; row < arr.length; row++) {
             for (int column = 0; column < arr[row].length; column++) {
